0599461a116f9197bac1f4cc80a3c98053aafa48
chaipa <chaitanya.patel@flemingcollege.ca>
2020-07-06 23:37:47 -0400

    first portfolio

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Layout, Header, Drawer, Navigation, Content } from 'react-mdl';
+import 'react-mdl/extra/material.css';
+import {Link} from  "react-router-dom"
+
+import Main from "./components/Main"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-      </header>
-    </div>
+    <div className="demo-big-content">
+    <Layout>
+        <Header title="Title" scroll>
+            <Navigation>
+                <Link to="/Resume">Resume</Link>
+                <Link to="/Projects">Projects</Link>
+                <Link to="/Aboutme">About Me</Link>
+                <Link to="/Contact">Contact</Link>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <Link to="/Resume">Resume</Link>
+                <Link to="/Aboutme">Projects</Link>
+                <Link to="/Aboutme">About Me</Link>
+                <Link to="/Contact">Contact</Link>
+            </Navigation>
+        </Drawer>
+        <Content>
+
+            <div className="page-content" />
+            <Main />
+
+
+        </Content>
+    </Layout>
+</div>
+
+
   );
 }
 
