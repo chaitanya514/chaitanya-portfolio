3286f7564ae3c16b726916e88476165d590ee5ba
chaipa <chaitanya.patel@flemingcollege.ca>
2020-08-05 16:13:09 -0400

    added flex on project page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,18 @@
 import React, { Component } from "react";
-import { Tabs, Tab } from "react-mdl";
+import "../App.css"
+import {
+    Tabs,
+    Tab,
+    Grid,
+    Cell,
+    Card,
+    CardTitle,
+    CardText,
+    CardActions,
+    CardMenu,
+    IconButton,
+    Button
+} from "react-mdl";
 
 class Projects extends Component {
     constructor(props) {
@@ -11,7 +24,43 @@ class Projects extends Component {
 
     toggleCategories() {
         if (this.state.activeTab === 0) {
-            return <div><h1>This is React</h1></div>
+            return (
+            
+            <div className="project-grid">
+
+                <Card shadow={0} style={{ width: '350px', height: '350px', margin: 'auto' }}>
+                    <CardTitle expand style={{ color: '#fff', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/320px-React-icon.svg.png) bottom right 15% no-repeat #46B6AC' }}>React Project #1</CardTitle>
+                    <CardText>
+                        counter react project
+                </CardText>
+                    <CardActions border>
+                        <Button colored >GitHub Repo.</Button>
+                    </CardActions>
+                </Card>
+
+                <Card shadow={0} style={{ width: '350px', height: '350px', margin: 'auto' }}>
+                    <CardTitle expand style={{ color: '#fff', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/320px-React-icon.svg.png) bottom right 15% no-repeat #46B6AC' }}>React Project #1</CardTitle>
+                    <CardText>
+                        counter react project
+                </CardText>
+                    <CardActions border>
+                        <Button colored >GitHub Repo.</Button>
+                    </CardActions>
+                </Card>
+
+                <Card shadow={0} style={{ width: '350px', height: '350px', margin: 'auto' }}>
+                    <CardTitle expand style={{ color: '#fff', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/320px-React-icon.svg.png) bottom right 15% no-repeat #46B6AC' }}>React Project #1</CardTitle>
+                    <CardText>
+                        counter react project
+                </CardText>
+                    <CardActions border>
+                        <Button colored >GitHub Repo.</Button>
+                    </CardActions>
+                </Card>
+            </div>
+            
+        )
+            
         }
         else if (this.state.activeTab === 1) {
             return <div><h1>This is Redux</h1></div>
@@ -36,10 +85,14 @@ class Projects extends Component {
                     <Tab>Postgres</Tab>
                     <Tab>GraphQl</Tab>
                 </Tabs>
-                <section>
+                <section className="demo-tab">
                     <div className="content">Content for the tab: {this.state.activeTab}
+                        <Grid className="projects-grid">
+                            <Cell col={12}>
+                                <div className="content"> {this.toggleCategories()}</div>
+                            </Cell>
+                        </Grid>
 
-                        {this.toggleCategories()}
                     </div>
                 </section>
             </div>
